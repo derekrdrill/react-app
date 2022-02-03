568198c956d4ec69f65380e98d49f5b607bfafa3
DerekDrill <derek.drill@implan.com>
2022-02-03 14:04:03 -0500

    Section 2 Chapter 10: creating Animal class and inspecting

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import ReactDOM from 'react-dom'
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    speak() {
+        console.log(`I am ${this.name} and I am ${this.age} years old`);
+    }
+}
+
+const animal1 = new Animal('George', 2);
+animal1.speak();
+console.log(animal1);
