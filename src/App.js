88bcb5562cef2a29627320cd6545921a0803428f
DerekDrill <derek.drill@implan.com>
2022-02-03 14:05:18 -0500

    Section 2 Chapter 10: creating RegularClass and ComponentClass and inspecting their makeup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component} from 'react';
 
+class RegularClass {}
+class ComponentClass extends Component {}
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log(regularClassInstance);
+console.log(componentClassInstance);
 export default class App extends Component {
     render() {
         return (
             <div>
                 <h1>Hello!</h1>
                 <p>My name is Derek. I am  a web developer.</p>
-                <p>I'm looking forward to refreshing with this course</p>
+                <p>I am looking forward to refreshing with this course</p>
             </div>
         )
     }
