6924e033ec1934199fb48b1a147e13bc0947397d
DerekDrill <derek.drill@implan.com>
2022-02-03 14:04:43 -0500

    Section 2 Chapter 10: creating Lion class, that extends Animal class, and inspecting

--- src/index.js
+++ src/index.js
@@ -18,3 +18,20 @@ class Animal {
 const animal1 = new Animal('George', 2);
 animal1.speak();
 console.log(animal1);
+
+class Lion extends Animal {
+    constructor(name, age, furColor, speed) {
+        super(name, age);
+
+        this.furColor = furColor;
+        this.speed = speed;
+    }
+
+    roar() {
+        console.log(`ROAR! I have ${this.furColor} fur and I can run ${this.speed} mph!`);
+    }
+}
+
+const lion1 = new Lion('George', 2, 'chocolate and white', 55);
+lion1.speak();
+lion1.roar();
