d9f399638836804a3c02e2d5e2dec88f35a885e6
DerekDrill <derek.drill@implan.com>
2022-02-03 13:11:36 -0500

    Section 2 Cjapter 8: refactoring how App is exported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 
-class App extends Component {
+export default class App extends Component {
     render() {
         return (
             <div>
@@ -12,4 +12,3 @@ class App extends Component {
     }
 }
 
-export default App;
