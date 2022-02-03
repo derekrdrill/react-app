dc997dff66ce053336694dd68b9bd4b9d6019213
DerekDrill <derek.drill@implan.com>
2022-02-03 13:00:00 -0500

    Section 2 Chapter 8

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Derek. I am  a web developer.</p>
+                <p>I'm looking forward to refreshing with this course</p>
+            </div>
+        )
+    }
+}
+
+export default App;
