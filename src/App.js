52cc205025ff104b8a43bb79a3a8bbd6ed8f2e58
DerekDrill <derek.drill@implan.com>
2022-02-04 11:47:34 -0500

    Section 2 Chapter 19: replacing html with ProfileImage and Bio components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React, { Component} from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
-import profile from './assets/profile.png';
+import ProfileImage from './ProfileImage';
+import Bio from './Bio';
 
 export default class App extends Component {
-    state = { displayBio: false };
-
-    toggleDisplayBio = () => { 
-        this.setState({ displayBio: !this.state.displayBio });
-    }
-
     render() {
         return (
             <div>
-                <img src={profile} alt='profile' />
-                <h1>Hello!</h1>
-                <p>My name is Derek. I am  a web developer.</p>
-                <p>I am looking forward to refreshing with this course</p>
-                {
-                    this.state.displayBio ? (
-                        <div>
-                            <p>I live in Charlotte, and code every day.</p>
-                            <p>My favorite languages are JavaScript and Typescript</p>
-                            <p>Besides coding, I love my dog, staying active, sports and cooking!</p>
-                            <button className='btn btn-primary btn-sm' onClick={this.toggleDisplayBio}>Show less</button>
-                        </div>
-                    ) : (
-                        <div>
-                            <button className='btn btn-primary btn-sm' onClick={this.toggleDisplayBio}>Read more</button>
-                        </div>
-                    )
-                }
+                <ProfileImage />
+                <Bio />
                 <hr />
                 <Projects />
                 <hr />
