1373a1ce51098f93d6b820dac13ffa764e711007
DerekDrill <derek.drill@implan.com>
2022-02-05 13:33:04 -0500

    Sec 4 Ch 32: Stateless component refactor (App)

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
-import React, { Component} from 'react';
+import React from 'react';
 import Projects from './Projects/components/Projects';
 import SocialProfiles from './SocialProfiles/components/SocialProfiles';
 import ProfileImage from './ProfileImage/components/ProfileImage';
 import Bio from './Bio/components/Bio';
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <ProfileImage />
-                <Bio />
-                <hr />
-                <Projects />
-                <hr />
-                <SocialProfiles />
-            </div>
-        )
-    }
-}
+const App = () => (
+    <div>
+        <ProfileImage />
+        <Bio />
+        <hr />
+        <Projects />
+        <hr />
+        <SocialProfiles />
+    </div>
+)
+
+export default App;
 
