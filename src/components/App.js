370261bb3592eb97b95608b53239f41fb47f164b
DerekDrill <derek.drill@implan.com>
2022-02-05 11:54:50 -0500

    Ch 4 Sec 31: refactoring component imports in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component} from 'react';
-import Projects from './Projects';
-import SocialProfiles from './SocialProfiles';
+import Projects from './Projects/components/Projects';
+import SocialProfiles from './SocialProfiles/components/SocialProfiles';
 import ProfileImage from './ProfileImage';
-import Bio from './Bio/Bio';
+import Bio from './Bio/components/Bio';
 
 export default class App extends Component {
     render() {
