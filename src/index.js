c476f7103f2dd8ceab52446257136aefe31f5473
Derek Drill <derekrdrill@gmail.com>
2022-02-07 21:44:40 -0500

    Sec 4 Ch 37: React Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
+import Jokes from './components/Jokes/components/Jokes';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={history}>
+        <Switch>
+            <Route exact={true} path='/' component={App} />
+            <Route path='/jokes' component={Jokes} />
+        </Switch>
+    </Router>, 
+    // null,
+    document.getElementById('root')
+);
